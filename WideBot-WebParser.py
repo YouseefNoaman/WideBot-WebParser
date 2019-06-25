70708fda9d8c033a23e188b85d24d641df40374e
YouseefNoaman <youseefnoaman@gmail.com>
2019-06-25 18:30:16 +0200

    this code has much better complexity and readability, now it is only 1 loop, and has only one error, it doesn't reiterate when the link is not found.

--- WideBot-WebParser.py
+++ WideBot-WebParser.py
@@ -15,85 +15,49 @@ URLS = []  # a list to hold the URLs to check if there is a duplicate
 
 
 def waitForThirty():  # this function will be used to make a 30 seconds interval
-    time.sleep(30)
+    time.sleep(2)
+
+
+def getLink(url):
+    response = requests.get(url)
+    print("response", response)
+    html = response.text
+    print("html", html)
+    soup = BeautifulSoup(html, "html.parser")
+
+    # print(soup)
+
+    link = soup.find('p')
+    print(link)
+    article_link = link.find("a", attrs={'href': re.compile("^/wiki/")})
+    print(article_link)
+    return article_link
 
 
 def find_philosophy(url):
-    MAX_HOPS = 100  # maximum number of clicks on links
-    count = 0  # number of hops
+    MAX_HOPS = 100
+    count = 0
 
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "lxml")  # souping the URL up and converting it to text using beautifulsoup
-    print(r.url)  # Print current url (after redirection)
-    URLS.append(r.url)
-    # the souping isn't working here, i think maybe Wikipedia stopping scraping by bots maybe, so i guess selenium
-    # should be used instead but there is no time for further modification
-    soup = soup.find("span", id='firstHeading')
-    soup = soup.text
-    print(soup)
-    while soup != 'Philosophy':
+    link = getLink(url)
+    print(link)
+
+    while '/wiki/Philosophy' not in link:
         if count == MAX_HOPS:
             print("MAX_HOPS reached.")
             return None
 
-        content = soup.find(id='mw-content-text')
-        for t in content.find_all(class_=['navbox', 'vertical-navbox', 'toc']):
-            t.replace_with("")
-
-        paragraph = soup.select('div#mw-content-text > p')[0]  # Only DIRECT child
-        for s in paragraph.find_all(
-                ['span', 'small', 'sup,', 'i', 'table']):  # remove spans and smalls with language, pronounciation
-            s.replace_with("")
-        paragraphText = str(paragraph)
-        paragraphText = re.sub(r' \(.*?\)', '', paragraphText)  # Remove leftover parenthesized text
-
-        # For debugging:
-        # print(paragraphText)
-
-        reParagraph = BeautifulSoup(paragraphText)  # back into bs4 object to find links
-        firstLink = reParagraph.find(href=re.compile('^/wiki/'))  # links that start with /wiki/ only
-
-        while firstLink == None:
-            # case of disambiguation: use first wiki link in list
-            if '(disambiguation)' in url or '(surname)' in url:
-                firstLink = content.ul.find(href=re.compile('^/wiki/'))
-
-            else:
-                paragraph = paragraph.find_next_sibling("p")
-
-                if paragraph is None:  # Catch-case
-
-                    if content.ul is not None:  # means if the scanned links in page has content
-                        firstLink = content.ul.find(href=re.compile('^/wiki/'))  # Disambiguation-type page
-                    if firstLink is None:  # No links available
-                        print("Wikipedia not reachable.")
-                        return None
-                    continue
-
-                for s in paragraph.find_all(['span', 'small', 'sup,', 'i', 'table']):
-                    s.replace_with("")
-                paragraphText = str(paragraph)
-                paragraphText = re.sub(r' \(.*?\)', '', paragraphText)
-                reParagraph = BeautifulSoup(paragraphText)
-                firstLink = reParagraph.find(href=re.compile('^/wiki/'))
-
-            # For debugging:
-            # print(paragraphText)
-
-        url = 'http://en.wikipedia.org' + firstLink.get('href')
-        print(url)
-        r = requests.get(url)  # Make new request
-        soup = BeautifulSoup(r.text)  # Soup it up again
-
+        link = getLink(link)
         count = count + 1
 
         waitForThirty()
-        if isDuplicate(r.url, URLS):
+        if isDuplicate(link, URLS):
             print("we are in a loop")
-            break;
 
     print(str(count) + " hops")
     return count
+
+
+#
 
 
 def isDuplicate(URL,
@@ -104,7 +68,7 @@ def isDuplicate(URL,
         return False
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # this is used to run the code internally, to also import the file and use it by other people
     urlRandom = 'http://en.wikipedia.org/wiki/Special:Random'
 
     # sys.argv is automatically a list of strings representing the arguments, to get the number of them use len()
